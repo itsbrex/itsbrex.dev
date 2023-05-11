cc8ff0027242afca332607e424463322df2d8b31
itsbrex <roach7@gmail.com>
2023-05-10 22:45:01 -0700

    ✅  FEAT: updated particle effects

--- app/page.tsx
+++ app/page.tsx
@@ -28,26 +28,22 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				itsbrex
+				@itsbrex
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-lg text-zinc-500 ">
-					Hi, my name is Brian. I'm a Technical Product Manager and Software
-					Engineer.
+					Hi, I'm Brian, a Technical Product Manager with <strong>over a decade</strong> of experience.
 					<br />
-					Fresh off back surgery and open new opportunities.
-					<br />
-					Please{" "}
+					🏥 Fresh off back surgery and{" "}
 					<Link
 						target="_blank"
 						href="https://linkedin.com/in/itsbrianroach"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						reach out
+						open new opportunities.
 					</Link>{" "}
-					if you think I may be a good fit on your team!
 				</h2>
 			</div>
 		</div>
