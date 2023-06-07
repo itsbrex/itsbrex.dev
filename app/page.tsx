e8a5bb9355010e9fcf31a96b409b1e563cc4d13b
itsbrex <roach7@gmail.com>
2023-06-06 17:29:39 -0700

    added a few more of many many more projects, lol

--- app/page.tsx
+++ app/page.tsx
@@ -27,22 +27,22 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+			<h1 className="z-10 text-4xl text-transparent align-middle duration-1000 cursor-default bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
 				@itsbrex
 			</h1>
 
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+			<div className="hidden w-screen h-px align-middle animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-lg text-zinc-500 ">
-					Hi, I'm Brian, a Technical Product Manager with <strong>over a decade</strong> of experience.
+					Hi, I'm Brian, a Technical PM & Software Engineer with <strong>over a decade</strong> of experience.
 					<br />
-					🏥 Fresh off back surgery and{" "}
+					Actively looking for my next{" "}
 					<Link
 						target="_blank"
 						href="https://linkedin.com/in/itsbrianroach"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						open new opportunities.
+					opportunity.
 					</Link>{" "}
 				</h2>
 			</div>
